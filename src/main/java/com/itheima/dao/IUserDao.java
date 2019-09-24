51665edc2c8d4942f94c6d40d1f9a749f496efa8
salt4172crp <wangyan4174@163.com>
2019-09-24 10:24:21 +0800

    #for class

--- src/main/java/com/itheima/dao/IUserDao.java
+++ src/main/java/com/itheima/dao/IUserDao.java
@@ -1,6 +1,7 @@
 package com.itheima.dao;
 
 import com.itheima.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -16,5 +17,6 @@ public interface IUserDao {
      * 查询所有操作
      * @return
      */
+    @Select("select * from user")
     List<User> findAll();
 }
